abe8e433328c113a48bd5ced0eac5e37229b9faa
ClaytonPerras <claytonperras@gmail.com>
2021-03-12 17:02:19 -0500

    Added a contact form. Fixed some UI responsiveness issues.

--- gulp-coffee/script.js
+++ gulp-coffee/script.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
+console.log("Hello")
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
